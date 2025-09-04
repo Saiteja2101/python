b2d5e56e17d8d5d81f6efb790b2be6b14a81ea9c
Saiteja2101 <bsaiteja2101@gmail.com>
2025-09-04 15:10:31 +0200

    Testing Commit

--- pythonday1.py
+++ pythonday1.py
@@ -1,3 +1,4 @@
-Actor_name = input("Welcome to Bandnme Gnerator\nWho is your favorite Actor?")
+Actor_name = input("Welcome to Bandname Generator\nWho is your favorite Actor?")
 City_name = input("Where to do you live?")
 print("Your band name is "+ City_name+" "+Actor_name+"Club")
+
