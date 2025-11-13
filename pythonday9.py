306fde4ce5d1453245d67355e69d946e902f2f25
Saiteja2101 <bsaiteja2101@gmail.com>
2025-11-13 16:27:32 +0100

    Day 9 Learning

--- pythonday9.py
+++ pythonday9.py
@@ -1,31 +1,25 @@
+def find_biggest_bidder(bidding_dictionary):
+    winner = ""
+    highest_bid = 0
+    for bidder in bidding_dictionary:
+        bid_amount = bidding_dictionary[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}.")
 
+
+bids = {}
 game_over = False
 
 while not game_over:
-    name = input("What is  your name?\n")
-    price = input("What is your bid?\n$")
-
-    bids = {}
+    name = input("What is your name?\n")
+    price = int(input("What is your bid?\n$"))  # Convert to int
     bids[name] = price
 
-    should_continue = input("Are there any other bids? type yes or no\n")
+    should_continue = input("Are there any other bids? Type 'yes' or 'no'\n").lower()
     if should_continue == "no":
         game_over = True
         find_biggest_bidder(bids)
     elif should_continue == "yes":
-        print("\n"*20)
-
-
-def find_biggest_bidder(bidding_dictiony):
-    winner = ""
-    highest_bid = 0
-    max(bidding_dictiony)
-    for bidder in bidding_dictiony:
-        bid_amount = bidding_dictiony[bidder]
-        if bid_amount > highest_bid:
-            highest_bid = bid_amount
-            winner = bidder
-
-
-
-print(f"The winner is {winner} with a bid of ${highest_bid}.")
+        print("\n" * 20)  # Clears the screen
